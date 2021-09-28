fe94ae8c83bc068b1e294c1c669750afa4642198
Eduardo Tolentino <eduar2tc@gmail.com>
2021-09-28 03:22:36 -0500

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -4,16 +4,21 @@
 /*Define constant App root*/
 define('APP_ROOT', dirname( dirname(__FILE__) ) );  
 //define URL Root
-define('URL_ROOT', 'http://localhost/Shareposts');
+/*define('URL_ROOT', 'http://localhost/Shareposts');*/
+define('URL_ROOT', 'https://myshareposts.herokuapp.com');
 //Site Name
 define('SITE_NAME', 'Shareposts');
 
 //DB Params
-define('DB_HOST', 'localhost');
+/*define('DB_HOST', 'localhost');*/
+/*define('DB_USER', 'root');*/
+/*define('DB_PASS', 'ACM1PT');*/
+/*define('DB_NAME', 'sharepost');*/
+define('DB_HOST', 'us-cdbr-east-04.cleardb.com');
+define('DB_USER', 'b49a33ca991edc');
+define('DB_PASS', '0d848667');
+define('DB_NAME', 'heroku_1a79350df9cda7d');
 define('CHARSET', 'utf8');
-define('DB_USER', 'root');
-define('DB_PASS', 'ACM1PT');
-define('DB_NAME', 'sharepost');
 
 //Version site App
 define('APP_VERSION', '1.0.0');
